70dfbe8445ca245345133448f21e423906c3629b
vishal-gupta <vishalgupta28012004@gmail.com>
2025-09-16 10:25:00 +0530

    first commit without codeSpliting

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
+import More from "./More.jsx";
+import Home from "./Home.jsx";
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/more",
+    element: <More />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={routers} />
+  </StrictMode>
+);
